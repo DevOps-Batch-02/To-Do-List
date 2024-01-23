cad2ebb395c73bc5123318bbf9cef0bc7776897d
jejidurga08 <147016097+jejidurga08@users.noreply.github.com>
2024-01-23 19:31:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,93 +1,46 @@
-const form = document.querySelector('#form')
-const username = document.querySelector('#username');
-const email = document.querySelector('#email');
-const password = document.querySelector('#password');
-const cpassword = document.querySelector('#cpassword');
+function solve() { 
+	let password = 
+		document.getElementById('password').value; 
+	let repassword = 
+		document.getElementById('repassword').value; 
+	let mobile = 
+		document.getElementById('mobile').value; 
+	let mail = 
+		document.getElementById('email').value; 
+	let flag = 1; 
+	let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
 
-form.addEventListener('submit',(e)=>{
-    
-    if(!validateInputs()){
-        e.preventDefault();
-    }
-})
+	if (!emailRegex.test(mail)) { 
+		flag = 0; 
+		pass.innerText = 
+			'Please enter a valid email address.'; 
+		setTimeout(() => { 
+			pass.innerText = ""; 
+		}, 3000); 
+	} 
 
-function validateInputs(){
-    const usernameVal = username.value.trim()
-    const emailVal = email.value.trim();
-    const passwordVal = password.value.trim();
-    const cpasswordVal = cpassword.value.trim();
-    let success = true
+	if (password !== repassword) { 
+		flag = 0; 
+		pass.innerText = 
+			"Passwords do not match. Please re-enter."; 
+		setTimeout(() => { 
+			pass.innerText = ""; 
+		}, 3000); 
+	} 
 
-    if(usernameVal===''){
-        success=false;
-        setError(username,'Username is required')
-    }
-    else{
-        setSuccess(username)
-    }
+	let passwordRegex = 
+		/^(?=.*\d)(?=.*[a-zA-Z])(?=.*[^a-zA-Z0-9])\S{8,}$/; 
 
-    if(emailVal===''){
-        success = false;
-        setError(email,'Email is required')
-    }
-    else if(!validateEmail(emailVal)){
-        success = false;
-        setError(email,'Please enter a valid email')
-    }
-    else{
-        setSuccess(email)
-    }
-
-    if(passwordVal === ''){
-        success= false;
-        setError(password,'Password is required')
-    }
-    else if(passwordVal.length<8){
-        success = false;
-        setError(password,'Password must be atleast 8 characters long')
-    }
-    else{
-        setSuccess(password)
-    }
-
-    if(cpasswordVal === ''){
-        success = false;
-        setError(cpassword,'Confirm password is required')
-    }
-    else if(cpasswordVal!==passwordVal){
-        success = false;
-        setError(cpassword,'Password does not match')
-    }
-    else{
-        setSuccess(cpassword)
-    }
-
-    return success;
-
+	if (!passwordRegex.test(password)) { 
+		flag = 0; 
+		pass.innerText = 
+			'Password must be at least 8 characters'+ 
+			' long and include at least one number,'+ 
+			' one alphabet, and one symbol.'; 
+		setTimeout(() => { 
+			pass.innerText = ""; 
+		}, 3000); 
+	} 
+	if (flag) 
+		alert("Form submitted"); 
 }
-//element - password, msg- pwd is reqd
-function setError(element,message){
-    const inputGroup = element.parentElement;
-    const errorElement = inputGroup.querySelector('.error')
-
-    errorElement.innerText = message;
-    inputGroup.classList.add('error')
-    inputGroup.classList.remove('success')
-}
-
-function setSuccess(element){
-    const inputGroup = element.parentElement;
-    const errorElement = inputGroup.querySelector('.error')
-
-    errorElement.innerText = '';
-    inputGroup.classList.add('success')
-    inputGroup.classList.remove('error')
-}
-
-const validateEmail = (email) => {
-    return String(email)
-      .toLowerCase()
-      .match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      );
-  };
